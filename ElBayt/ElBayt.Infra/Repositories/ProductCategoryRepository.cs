0ea0e4d124bb5acf3af1b1c5c37702124b235c19
Abdelrhman <Abdelrhman@DESKTOP-PT50KDS>
2021-08-18 10:16:48 -0700

    Repositories

---
+++ ElBayt/ElBayt.Infra/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ElBayt.Common.Infra.Common;
+using ElBayt.Common.Infra.Mapping;
+using ElBayt.Common.Mapping;
+using ElBayt.Core.Entities;
+using ElBayt.Core.IRepositories;
+using ElBayt.Infra.Context;
+using ElBayt.Infra.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElBayt.Infra.Repositories
+{
+    public class ProductCategoryRepository : GenericRepository<ProductCategoryEntity, ProductCategoryModel>, IProductCategoryRepository
+    {
+        private readonly ElBaytContext _dbContext;
+        private readonly ITypeMapper _mapper;
+        
+
+        public ProductCategoryRepository(ElBaytContext dbContext, ITypeMapper mapper) : base(dbContext, mapper)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _mapper = mapper;
+        }      
+    }
+}
